7e47a5b350bb06fbb1aa73f94acdd7c92e956af0
Alex Espino <alex.matt.espino@gmail.com>
2019-04-07 20:34:31 -0700

    Add Exercise 1 code

---
+++ Lab 2/ahern236_lab2_part1/ahern236_lab2_part1/main.c
@@ -0,0 +1,43 @@
+/*
+ * ahern236_lab2_part1.c
+ *
+ * Created: 4/7/2019 7:11:31 PM
+ * Author : alex.espino
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+	// Initialize I/O Ports
+    DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+    DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+	
+	// Input temp vars
+	unsigned char tmpA1 = 0x00; // Temporary variable to hold the value of A1 (Light Sensor Input)
+	unsigned char tmpA0 = 0x00; // Temporary variable to hold the value of A0 (Garage Door Sensor Input)
+	
+	//Output temp vars
+    unsigned char tmpB0 = 0x00; // Temporary variable to hold the value of B0 (LED Output)
+	
+    while(1)
+    {
+	    // Read input
+	    tmpA1 = PINA & 0x02; // reads from PINA: 000000b0
+		tmpA0 = PINA & 0x01; // reads from PINA: 0000000b
+		
+	    // if PA1 is 0 and PA0 is 1, set PB0 = 1, else = 0
+	    if ((tmpA1 == 0x00) && (tmpA0 == 0x01)) { // True if PA0 is 1
+		    tmpB0 = (tmpB0 & 0xFE) | 0x01; // Sets tmpB to bbbbbbb1
+		    // (clear rightmost bit, then set to 1)
+	    }
+	    else {
+		    tmpB0 = (tmpB0 & 0xFE); // Sets tmpB to bbbbbbb0
+	    }
+		
+	    // Write output
+	    PORTB = tmpB0;
+    }
+    return 0;
+
+}
