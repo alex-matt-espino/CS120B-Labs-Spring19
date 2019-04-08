b73643fbcc1182b5a662c3805ec30286c8303035
Alex Espino <alex.matt.espino@gmail.com>
2019-04-07 21:40:24 -0700

    Add part 2 and part 3 code

---
+++ Lab 2/ahern236_lab2_part2/ahern236_lab2_part2/main.c
@@ -0,0 +1,50 @@
+/*
+ * ahern236_lab2_part2.c
+ *
+ * Created: 4/7/2019 8:38:23 PM
+ * Author : alex.espino
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    // Initialize I/O Ports
+    DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+    DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+	
+	// Input temp vars
+	unsigned char pSensor3 = 0x00;
+	unsigned char pSensor2 = 0x00;
+	unsigned char pSensor1 = 0x00;
+	unsigned char pSensor0 = 0x00;
+	
+	// Output temp vars
+	unsigned char cntavail = 0x00; // The number of available spots
+	
+    while (1) 
+    {
+		// Read input
+		pSensor3 = PINA & 0x08; // sets pSensor3 to 0000b000
+		pSensor2 = PINA & 0x04; // sets pSensor2 to 00000b00
+		pSensor1 = PINA & 0x02; // sets pSensor1 to 000000b0
+		pSensor0 = PINA & 0x01; // sets pSensor0 to 0000000b
+		
+		cntavail = 0x04; // Initialize avail spots to 4
+		
+		if(pSensor3 == 0x08)
+			cntavail--;
+		if(pSensor2 == 0x04)
+			cntavail--;
+		if(pSensor1 == 0x02)
+			cntavail--;
+		if(pSensor0 == 0x01)
+			cntavail--;
+		
+		// Write output
+		PORTB = cntavail;
+    }
+	return 0;
+}
+
---
+++ Lab 2/ahern236_lab2_part3/ahern236_lab2_part3/main.c
@@ -0,0 +1,62 @@
+/*
+ * ahern236_lab2_part3.c
+ *
+ * Created: 4/7/2019 9:04:12 PM
+ * Author : alex.espino
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+	// Initialize I/O Ports
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs, initialize to 0s
+	
+	// Input temp vars
+	unsigned char pSensor3 = 0x00;
+	unsigned char pSensor2 = 0x00;
+	unsigned char pSensor1 = 0x00;
+	unsigned char pSensor0 = 0x00;
+	
+	// Output temp vars
+	unsigned char cntavail = 0x00; // The number of available spots
+	unsigned char tmpC = 0x00; // Temp var for port C
+	
+	while (1)
+	{
+		// Read input
+		pSensor3 = PINA & 0x08; // sets pSensor3 to 0000b000
+		pSensor2 = PINA & 0x04; // sets pSensor2 to 00000b00
+		pSensor1 = PINA & 0x02; // sets pSensor1 to 000000b0
+		pSensor0 = PINA & 0x01; // sets pSensor0 to 0000000b
+		
+		cntavail = 0x04; // Initialize avail spots to 4
+		
+		if(pSensor3 == 0x08)
+			cntavail--;
+		if(pSensor2 == 0x04)
+			cntavail--;
+		if(pSensor1 == 0x02)
+			cntavail--;
+		if(pSensor0 == 0x01)
+			cntavail--;	
+		
+		if(cntavail == 0x00)
+		{	
+			tmpC = 0x80;
+		}
+		else
+		{
+			tmpC = cntavail;
+		}
+		
+		// Write output
+		PORTB = cntavail;
+		PORTC = tmpC;
+		
+		
+	}
+	return 0;
+}
